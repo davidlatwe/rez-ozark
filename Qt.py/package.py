2be978cfdb76597bd8476299ddc7bc5f92efe632
davidlatwe <davidlatwe@gmail.com>
2020-08-07 01:11:25 +0800

    Add Qt.py rez package

--- Qt.py/package.py
+++ Qt.py/package.py
@@ -1,25 +1,113 @@
+early = globals()["early"]
 
 name = "Qt.py"
 
 uuid = "repository.Qt.py"
 
-version = ""
+
+@early()
+def __latest():
+    import os
+    import json
+    import shutil
+    import tempfile
+    from subprocess import check_output, CalledProcessError
+
+    if os.getenv("GIT_CLONED"):
+        # Reuse data from previous clone
+        data = json.loads(os.environ["GIT_CLONED"])
+        return data
+
+    url = "https://github.com/mottosso/Qt.py.git"
+
+    tmpdir = tempfile.mkdtemp()
+    tempdir = os.path.join(tmpdir, "Qt.py")
+    try:
+        check_output(["git", "clone", "--single-branch", url, tempdir])
+        check_output(["git", "fetch", "--tags"], cwd=tempdir)
+        commit = check_output(["git", "rev-list", "--tags", "--max-count=1"],
+                              cwd=tempdir).strip().decode()
+        tag = check_output(["git", "describe", "--tags", commit],
+                           cwd=tempdir).strip().decode()
+        check_output(["git", "checkout", tag], cwd=tempdir)
+
+    except CalledProcessError:
+        shutil.rmtree(tmpdir)
+        raise
+
+    data = {
+        "temp": tmpdir,
+        "repo": tempdir,
+        "tag": tag,
+    }
+    # Avoid repeating in each variation build
+    os.environ["GIT_CLONED"] = json.dumps(data)
+
+    return data
+
+
+@early()
+def version():
+    this = globals()["this"]
+    return this.__latest["tag"]
+
 
 description = "Minimal Python 2 & 3 shim around all Qt bindings - "\
               "PySide, PySide2, PyQt4 and PyQt5."
 
-authors = []
 
-variants = [
-    ["PyQt5"],
-    ["PySide2"],
-    ["PyQt4"],
-    ["PySide"],
-]
+@early()
+def authors():
+    from subprocess import check_output
 
+    this = globals()["this"]
+    repo = this.__latest["repo"]
+
+    name_list = check_output(["git", "shortlog", "-sn"],
+                             cwd=repo).strip().decode()
+    contributors = [n.strip().split("\t", 1)[-1]
+                    for n in name_list.split("\n")]
+    contributors.append("davidlatwe")
+
+    return contributors
+
+
+def __has_package(pkg_request):
+    from rez.packages import get_latest_package_from_string
+    return get_latest_package_from_string(pkg_request)
+
+
+@early()
+def variants():
+    import os
+    import shutil
+
+    bindings = [
+        "PyQt5",
+        "PySide2",
+        "PyQt4",
+        "PySide",
+    ]
+    variants_ = [
+        [binding] for binding in bindings if __has_package(binding)
+    ]
+    if not variants_:
+        # cleanup
+        this = globals()["this"]
+        shutil.rmtree(this.__latest["temp"])
+
+        raise Exception("No Qt binding package found.")
+
+    os.environ["REZ_BUILD_VARIANT_COUNT"] = str(len(variants_))
+
+    return variants_
+
+
+# build_requires = ["python-3"]  # For author name (unicode)
 
 build_command = "python {root}/rezbuild.py {install}"
 
 
 def commands():
+    env = globals()["env"]
     env.PYTHONPATH.prepend("{root}/payload")
--- Qt.py/rezbuild.py
+++ Qt.py/rezbuild.py
@@ -1,5 +1,57 @@
 
-# (TODO)
-# * build with git clone
-# Or find a way to download the latest release (tag)
+import os
+import sys
+import shutil
 
+
+def logger():
+    import logging
+
+    package_name = os.environ["REZ_BUILD_PROJECT_NAME"]
+    log_name = package_name + ".build"
+
+    # (TODO) Add formatter
+
+    log_handler = logging.StreamHandler()
+    log = logging.getLogger(log_name)
+    log.addHandler(log_handler)
+    log.setLevel(logging.INFO)
+
+    return log
+
+
+def build(source_path, build_path, install_path, targets=None):
+    import json
+
+    log = logger()
+    targets = targets or []
+
+    if "install" in targets:
+        dst = install_path + "/payload"
+    else:
+        dst = build_path + "/payload"
+
+    dst = os.path.normpath(dst)
+
+    if os.path.isdir(dst):
+        shutil.rmtree(dst)
+    os.makedirs(dst)
+
+    # start building ...
+    data = json.loads(os.environ["GIT_CLONED"])
+
+    shutil.copy2(os.path.join(data["repo"], "Qt.py"),
+                 os.path.join(dst, "Qt.py"))
+
+    index = int(os.environ["REZ_BUILD_VARIANT_INDEX"])
+    last = int(os.environ["REZ_BUILD_VARIANT_COUNT"]) - 1
+    if index == last:
+        # last one, do cleanup
+        shutil.rmtree(data["temp"])
+
+
+if __name__ == "__main__":
+    build(source_path=os.environ["REZ_BUILD_SOURCE_PATH"],
+          build_path=os.environ["REZ_BUILD_PATH"],
+          install_path=os.environ["REZ_BUILD_INSTALL_PATH"],
+          targets=sys.argv[1:])
