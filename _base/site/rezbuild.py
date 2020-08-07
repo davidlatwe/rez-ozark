55967b0a0a5dd4db781fa4261a8fd635e3d58213
David Lai <davidlatwe@gmail.com>
2020-08-07 23:09:48 +0800

    cleanup unused

--- _base/site/rezbuild.py
+++ _base/site/rezbuild.py
@@ -4,24 +4,7 @@ import sys
 import shutil
 
 
-def logger():
-    import logging
-
-    package_name = os.environ["REZ_BUILD_PROJECT_NAME"]
-    log_name = package_name + ".build"
-
-    # (TODO) Add formatter
-
-    log_handler = logging.StreamHandler()
-    log = logging.getLogger(log_name)
-    log.addHandler(log_handler)
-    log.setLevel(logging.INFO)
-
-    return log
-
-
 def build(source_path, build_path, install_path, targets=None):
-    log = logger()
     targets = targets or []
 
     if "install" in targets:
