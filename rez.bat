6cf0017682365ab00987ef87f8a6b33c56faa81b
davidlatwe <davidlatwe@gmail.com>
2020-08-11 00:39:39 +0800

    Drop shell scripts

--- rez.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set PATH=%USERPROFILE%\rez\core\Scripts\rez;%PATH%
-set REZ_CONFIG_FILE=%~dp0rezconfig.py
-call python .\rez_.py %*
--- rez.ps1
+++
@@ -1,3 +0,0 @@
-$env:PATH="$env:USERPROFILE\rez\core\Scripts\rez;$env:PATH"
-$env:REZ_CONFIG_FILE="$PSScriptRoot\rezconfig.py"
-python .\rez_.py %*
