fcc1b1fb60ace5f17ff6116588e6145d74f694c0
davidlatwe <davidlatwe@gmail.com>
2020-08-18 15:57:01 +0800

    Add app mocking tool

---
+++ mockapp/python/mockapp/__main__.py
@@ -0,0 +1,21 @@
+
+
+def mock(appname):
+    from Qt import QtWidgets
+
+    app = QtWidgets.QApplication(sys.argv)
+    button = QtWidgets.QPushButton(appname)
+    button.show()
+    app.exec_()
+
+
+if __name__ == "__main__":
+    import sys
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Mock App")
+    parser.add_argument("appname", help="App name to mock")
+
+    opt, unknown = parser.parse_known_args()
+
+    sys.exit(mock(opt.appname))
